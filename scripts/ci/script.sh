6f745e97af6ddb4fc7ee13eabe3997c55bf95b3e
Bradley J Chambers <brad.chambers@gmail.com>
2018-05-30 20:38:34 -0400

    Simplify CI script for now, update for non-Docker path

--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -4,8 +4,6 @@
 gcc --version
 g++ --version
 
-cd /pdal
-
 mkdir -p _build || exit 1
 cd _build || exit 1
 
@@ -32,12 +30,3 @@ make -j2
 LD_LIBRARY_PATH=./lib
 ctest -V
 make install
-
-for EXAMPLE in writing writing-filter writing-kernel writing-reader writing-writer
-do
-    cd /pdal/examples/$EXAMPLE
-    mkdir -p _build || exit 1
-    cd _build || exit 1
-    cmake -G "Unix Makefiles" .. && \
-    make
-done
