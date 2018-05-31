8bf1dfa53a700ffb35e903a9a309fff2d9e99f3f
Bradley J Chambers <brad.chambers@gmail.com>
2018-05-30 20:09:55 -0400

    Begin experimenting with a Conda-based Travis setup

--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -1,61 +1,6 @@
 #!/bin/sh -e
 # Builds and tests PDAL
 
-echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-echo "@edgecommunity http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories; \
-apk update
-apk add \
-    cmake \
-    alpine-sdk \
-    eigen-dev@edgecommunity \
-    hexer \
-    hexer-dev \
-    nitro \
-    nitro-dev \
-    gdal \
-    gdal-dev \
-    geos \
-    geos-dev \
-    laz-perf \
-    laz-perf-dev \
-    libexecinfo \
-    libexecinfo-dev \
-    libgeotiff \
-    libgeotiff-dev \
-    libxml2 \
-    libxml2-dev \
-    python \
-    python-dev \
-    py-numpy \
-    py-numpy-dev \
-    cython \
-    cython-dev \
-    py-pip \
-    jsoncpp \
-    jsoncpp-dev \
-    hdf5 \
-    hdf5-dev \
-    proj4 \
-    proj4-dev \
-    cpd \
-    cpd-dev \
-    fgt \
-    fgt-dev \
-    sqlite \
-    sqlite-dev \
-    postgresql-dev \
-    libcurl \
-    curl-dev \
-    linux-headers \
-    laszip \
-    laszip-dev \
-    libspatialite \
-    libspatialite-dev \
-    xz-dev \
-    xz-libs \
-    zstd \
-    zstd-dev \
-
 gcc --version
 g++ --version
 
@@ -87,15 +32,6 @@ make -j2
 LD_LIBRARY_PATH=./lib
 ctest -V
 make install
-
-# Python extension testing
-pip install packaging
-git clone https://github.com/PDAL/python.git pdal-python
-cd pdal-python
-python setup.py build
-echo "current path: " `pwd`
-export PDAL_TEST_DIR=/pdal/_build/test
-python setup.py test
 
 for EXAMPLE in writing writing-filter writing-kernel writing-reader writing-writer
 do
